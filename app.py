e79b97535d737d23ceb1a46e066e07b84c393fca
srm36524 <168128093+srm36524@users.noreply.github.com>
2026-07-16 21:25:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
+from scipy.optimize import minimize
 
 # ----------------------------------------------------
 # Page Configuration
